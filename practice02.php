d7190f185f2ab73013276cac1dc0ce2155c07e7f
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-18 02:38:53 +1000

    Update to use decision structures

--- practice02.php
+++ practice02.php
@@ -1,7 +1,68 @@
 <?php
 
+$is_dog = true;
+$humans = ["Dannielle", "Nick"];
+$dogs = ["Scarlet", "Reeba"];
 
-;?>
+$test_name = "Reeba";
+
+// Decision structures - if, else if, else
+if ($test_name == "Dannielle") {
+    echo "That is me!";
+} elseif ($test_name == "Reeba") {
+    echo "That is my dog";
+} else {
+    echo "It is not me or my dog";
+}
+
+// Decision structure - switch
+switch ($test_name) {
+    case "Dannielle":
+        echo "That is me!";
+        break;
+    case "Reeba":
+        echo "That is my dog";
+        break;
+    default:
+        echo "It is not me or my dog";
+}
+
+// Decision structure - ternary operator
+$result = ($test_name == "Dannielle") ? "Me!" : "Who is it then?";
 
 
+?>
+<?php $member = 1; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Practice</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Practice 02 Output</h1>
+<section>
+    <div class="both">
+        <p>Both</p>
+    </div>
+
+    <?php if (!$member) : ?>
+        <div class="non-members">
+            <p>Non-members</p>
+        </div>
+    <?php endif; ?>
+
+
+    <?php if ($member) : ?>
+        <div class="members">
+            <p>Members</p>
+        </div>
+    <?php endif; ?>
+</section>
+
+<br>
 <a href="index.html">Return to Home</a>
+
+</body>
+</html>
