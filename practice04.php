2a4fd499f2c604110e8731c36a3db6e4eb34d030
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-04-18 03:49:07 +1000

    Update practice04.php to use functions

---
+++ practice04.php
@@ -0,0 +1,56 @@
+<?php
+echo "PHP Version: " . phpversion();
+echo '<br>';
+
+// Upper, lower and Capital case
+$first_name = "dannielle";
+echo strtoupper($first_name);
+echo '<br>';
+$fixed_name = ucfirst(strtolower($first_name));
+echo $fixed_name;
+echo '<br>';
+
+// Sort
+$hobbies = array('Writing', 'Singing', 'Gaming');
+sort($hobbies);
+loop_through_array($hobbies);
+rsort($hobbies);
+loop_through_array($hobbies);
+
+// Function - pass by value
+function loop_through_array($my_items): void
+{
+    foreach ($my_items as $item) {
+        echo $item . '<br>';
+    }
+}
+
+$numbers = array(1, 2, 3);
+loop_through_array($numbers);
+loop_through_array_ref($numbers);
+
+// Function - pass by reference
+function loop_through_array_ref(&$my_items): void
+{
+    foreach ($my_items as $item) {
+        $item += 2;
+        echo $item . '<br>';
+    }
+}
+
+$pets_to_breeds = [['Name' => 'Reeba', "Breed" => 'Border Collie x Koolie'], ['Name' => 'Scarlet', "Breed" => 'Border Collie x Kelpie']];
+
+// Anonymous Function
+usort($pets_to_breeds, function($breed1, $breed2) {
+    return $breed1['Breed'] <=> $breed2['Breed'];
+});
+
+foreach ($pets_to_breeds as $pet_to_breed) {
+    echo '<li>' . implode(" ", $pet_to_breed) . '</li>';
+}
+?>
+<br>
+<h1>Practice 04 Output Above</h1>
+
+
+<a href="index.html">Return to Home</a>
